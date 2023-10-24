16e89b72c52ed9a7385eda400322304cd0423b62
snix119012 <aneta.mlynarczyk@student.uj.edu.pl>
2023-10-24 14:28:20 +0200

    .

--- LeapYear/src/main/java/me/aneta/Main.java
+++ LeapYear/src/main/java/me/aneta/Main.java
@@ -1,9 +1,17 @@
 package me.aneta;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("xdddddddddddddddddd");
+
+        LeapYear myObject = new LeapYear();
+        int year = 2024;
+        boolean isLeap = LeapYear.isLeapYear(year);
+        if (isLeap) {
+            System.out.println(year + " is leap year.");
+        } else {
+            System.out.println(year + " is not leap year");
+        }
+
+
     }
 }
