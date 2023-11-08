f8adf36e930c592311ab1eb24ea3ea9195eab039
snix119012 <aneta.mlynarczyk@student.uj.edu.pl>
2023-11-08 15:55:31 +0100

    leapyear

---
+++ LifeSimulator/src/main/java/me/aneta/Organism.java
@@ -0,0 +1,49 @@
+package me.aneta;
+
+import java.util.Random;
+
+public class Organism {
+
+    private int energy;
+    private Random rand = new Random();
+    private Position position;
+    private char symbol;
+    private Random random = new Random();
+    public Organism(int energy) {
+        this.energy = energy;
+        this.symbol ='O';
+    }
+
+    public void move(Board board) {
+        int newX = position.getX();
+        int newY = position.getY();
+        boolean moveVertically = random.nextBoolean();
+
+        if (moveVertically) {
+            // Move up or down by 1
+            newY += random.nextBoolean() ? 1 : -1;
+        } else {
+            // Move left or right by 1
+            newX += random.nextBoolean() ? 1 : -1;
+        }
+
+
+
+        board.moveOrganism(this,newX,newY);
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+}
